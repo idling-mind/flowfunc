42fba54f356ca9988cb6918ec15e1823c32314bb
Najeem Muhammed <najeem@gmail.com>
2022-09-12 21:39:52 +0200

    test_process_node_docstring

---
+++ tests/methods.py
@@ -0,0 +1,46 @@
+from typing import Tuple
+
+
+def add_with_docstring(a, b):
+    """Add two numbers together
+
+    Parameters
+    ----------
+    a: int
+        First number
+    b: int
+        Second number
+    
+    Returns
+    -------
+    sum: int
+        Sum of two numbers
+    """
+    return a + b
+
+def add_with_inspect(a: int, b: int) -> int:
+    """Add two numbers together"""
+    return a + b
+
+def sumnprod_with_docstring(a, b):
+    """Sum and product of two numbers
+
+    Parameters
+    ----------
+    a: int
+        First number
+    b: int
+        Second number
+    
+    Returns
+    -------
+    sum: int
+        Sum of two numbers
+    prod: int
+        Product of two numbers
+    """
+    return a + b, a * b
+
+def sumnprod_with_inspect(a: int, b: int) -> Tuple[int, int]:
+    """Sum and product of two numbers"""
+    return a + b, a * b
