18f5945e813f3502858efcd62174513ee6941dcd
Najeem Muhammed <najeem@gmail.com>
2024-09-23 22:20:31 +0200

    Refactor code to use safe subclass check in config.py and utils.py

--- flowfunc/utils.py
+++ flowfunc/utils.py
@@ -1,3 +1,14 @@
 import logging
 
 logger = logging.getLogger(__name__)
+
+
+def issubclass_safe(cls, classinfo):
+    """Check if a class is a subclass of another class.
+    Some classes like list, dict, etc. returns true when checked inspect.isclass
+    however, they raise TypeError when checked with issubclass.
+    """
+    try:
+        return issubclass(cls, classinfo)
+    except TypeError:
+        return False
